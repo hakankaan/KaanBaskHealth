bfcfd08d49d3d5b68d6127dafe182973bad79d66
Kaan Akın <kaanhakan2005@gmail.com>
2024-05-06 20:36:55 +0300

    fix: query client provider for test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { test, expect } from 'vitest';
 import Dashboard from './page';
+import { AppProvider } from '@/providers/app-provider';
 
 test('renders dashboard', () => {
-  render(<Dashboard />);
+  render(
+    <AppProvider>
+      <Dashboard />
+    </AppProvider>,
+  );
   expect(screen.getByText('Dashboard')).toBeDefined();
 });
