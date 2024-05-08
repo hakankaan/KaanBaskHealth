97d81b7db55a652fcf4874b244b840873383b854
Kaan Akın <kaanhakan2005@gmail.com>
2024-05-08 23:24:44 +0300

    chore: enabled ssr for production

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,9 @@
 import { Page as DashboardPage } from '@/modules/dashboard/ui/components/page/page';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard | Bask Health',
+};
 
 export default function Dashboard() {
   return <DashboardPage />;
